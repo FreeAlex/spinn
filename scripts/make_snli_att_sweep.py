913a693cbba1504659c671bd3af1d2f957f78cdd
Alex <xiaonan0429@gmail.com>
2017-04-11 15:10:56 -0400

    update sweep

--- scripts/make_snli_att_sweep.py
+++ scripts/make_snli_att_sweep.py
@@ -11,6 +11,7 @@ import sys
 LIN = "LIN"
 EXP = "EXP"
 SS_BASE = "SS_BASE"
+SS_SET = "SS_SET"
 
 FLAGS = gflags.FLAGS
 
@@ -18,13 +19,15 @@ gflags.DEFINE_string("training_data_path", "/home/xz1364/mlworkspace/snli_1.0/sn
 gflags.DEFINE_string("eval_data_path", "/home/xz1364/mlworkspace/snli_1.0/snli_1.0_dev.jsonl", "")
 gflags.DEFINE_string("embedding_data_path", "/home/xz1364/mlworkspace/glove/glove.840B.300d.txt", "")
 gflags.DEFINE_string("log_path", ".", "")
-gflags.DEFINE_string("exp_names", 'exp1,exp2', "experiment names, seperate by coma(,)")
-gflags.DEFINE_string("slurm_name", 'att-0', 'the file name of slurm output and err file')
-gflags.DEFINE_integer("mem", 8, "memory should be used for hpc for each task")
-gflags.DEFINE_string("spinn_path", '/home/xz1364/repos/faspinn/python', 'the model path so that spinn can run')
+gflags.DEFINE_string("exp_names", 'direct-6,direct-7', "experiment names, seperate by coma(,)")
+gflags.DEFINE_string("slurm_name", 'direct-6-7', 'the file name of slurm output and err file')
+gflags.DEFINE_integer("mem", 10, "memory should be used for hpc for each task")
+gflags.DEFINE_string("spinn_path", '/home/xz1364/repos/faspinn-dev3/python', 'the model path so that spinn can run')
 gflags.DEFINE_bool("using_diff_in_mlstm", True, 'wether or not use diff feature in mlstm')
 gflags.DEFINE_bool('using_prod_in_mlstm', True, 'wether or not use prod feature in mlstm')
 gflags.DEFINE_bool('using_null_in_attention', True, 'wether using null vector in attention, so that weights can assign to null vector')
+gflags.DEFINE_bool('using_only_mlstm_feature', True, 'wether using only mlstm feature in final represenation')
+gflags.DEFINE_bool('gpu', True, 'wether use GPU or not')
 FLAGS(sys.argv)
 
 # Instructions: Configure the variables in this block, then run
@@ -62,13 +65,22 @@ if FLAGS.using_prod_in_mlstm:
 if FLAGS.using_null_in_attention:
     FIXED_PARAMETERS['using_null_in_attention'] = ''
 
+if FLAGS.using_only_mlstm_feature:
+    FIXED_PARAMETERS['using_only_mlstm_feature'] = ''
+else:
+    FIXED_PARAMETERS['nousing_only_mlstm_feature'] = ''
+
 # Tunable parameters.
 SWEEP_PARAMETERS = {
-    "learning_rate":      ("lr", EXP, 0.0002, 0.002),  # RNN likes higher, but below 009.
+    "learning_rate":      ("lr", EXP, 0.0001, 0.0008),  # RNN likes higher, but below 009.
     "l2_lambda":          ("l2", EXP, 8e-7, 2e-5),
     "semantic_classifier_keep_rate": ("skr", LIN, 0.7, 0.95),  # NB: Keep rates may depend considerably on dims.
     "embedding_keep_rate": ("ekr", LIN, 0.7, 0.95),
     "learning_rate_decay_per_10k_steps": ("dec", EXP, 0.5, 1.0),
+    'mlp_dim': ('mlpd', SS_SET, [256, 512, 1024], 0),   # 0 is useless
+    'model_dim': ('mdim', SS_SET, [300, 400, 600], 0),  # 0 is useless
+    'num_mlp_layers': ('mlpl', SS_SET, [1,2], 0),
+
 }
 
 exp_names = FLAGS.exp_names.split(',')
@@ -76,18 +88,17 @@ exp_names = FLAGS.exp_names.split(',')
 
 def print_script_head():
     print '''#!/bin/sh
-
 # Generic job script for all experiments.
-
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
-#SBATCH --reservation=mhealth
-'''
+#SBATCH --reservation=mhealth'''
     print '#SBATCH --mem={}GB'.format(FLAGS.mem * len(exp_names))
     print '#SBATCH --output=slurm-{}-%j.out'.format(FLAGS.slurm_name)
     print '#SBATCH --error=slurm-{}-%j.err'.format(FLAGS.slurm_name)
     print '#SBATCH --job-name={}'.format(FLAGS.slurm_name)
+    if FLAGS.gpu:
+        print '#SBATCH --partition=gpu'
+        print '#SBATCH --gres=gpu:1'
+        print '#PRINCE PRINCE_GPU_COMPUTE_MODE=default'
     print '\n'
     print "# SWEEP PARAMETERS: " + str(SWEEP_PARAMETERS)
     print "# FIXED_PARAMETERS: " + str(FIXED_PARAMETERS)
@@ -100,13 +111,11 @@ pip install --user python-gflags==2.0
     print 'export PYTHONPATH=$PYTHONPATH:{}'.format(FLAGS.spinn_path)
     print '\n'
 
-def print_fixed_params():
+def get_fixed_params():
     params = FIXED_PARAMETERS
-    for param in params:
-        value = params[param]
-        print " --" + param + " " + str(value) + " \\"
+    return params
 
-def print_sweep_params():
+def get_sweep_params():
     params = {}
 
     for param in SWEEP_PARAMETERS:
@@ -124,6 +133,8 @@ def print_sweep_params():
             lmn = np.log(mn)
             lmx = np.log(mx)
             sample = 1 - np.exp(lmn + (lmx - lmn) * r)
+        elif t == SS_SET:
+            sample = random.choice(config[2])
         else:
             sample = mn + (mx - mn) * r
 
@@ -135,22 +146,28 @@ def print_sweep_params():
 
         params[param] = sample
 
-    for param in params:
-        value = params[param]
-        print " --" + param + " " + str(value) + " \\"
+    return params
+
 
 
 print_script_head()
 
 for exp_name in exp_names:
+    params = {}
     print 'echo running experiment {}'.format(exp_name)
-    print 'python -m spinn.models.fat_classifier  --noshow_progress_bar --gpu 0 \\'
+    print 'python -m spinn.models.fat_classifier  --noshow_progress_bar \\'
+    if FLAGS.gpu:
+        print ' --gpu 0 \\'
     print ' --experiment_name {} \\'.format(exp_name)
-    print_fixed_params()
-    print_sweep_params()
+    # update method will replace fixed params if it's in sweep params, BE CAREFUL if change the order
+    params.update(get_fixed_params())
+    params.update(get_sweep_params())
+    for param in params:
+        value = params[param]
+        print " --" + param + " " + str(value) + " \\"
     print ' & \\'
-    print '\n\n'
+    print '\n'
 
-print '\n'
+print ''
 print 'echo waiting'
 print 'wait'
